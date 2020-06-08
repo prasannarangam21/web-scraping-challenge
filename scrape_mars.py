135bb1ba38bd51b4ac5bffc3837b86c385ef0726
Lakshmi Prasanna Rangam <LakshmiPrasannaRangam>
2020-06-08 14:48:48 -0600

    added some code

--- scrape_mars.py
+++ scrape_mars.py
@@ -24,7 +24,11 @@ def featured_image(browser):
     # Visit the NASA JPL (Jet Propulsion Laboratory) Site
     url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
     browser.visit(url)
-
+    html = browser.html
+    soup = BeautifulSoup(html_image, 'html.parser')
+    image = soup.select_one("figure.lede a img").get("src")
+    image_url = f"https://www.jpl.nasa.gov{image}"
+    return image_url
     
    
    # Mars Weather Twitter Account Web Scraper
@@ -32,16 +36,39 @@ def twitter_weather(browser):
     # Visit the Mars Weather Twitter Account
     url = "https://twitter.com/marswxreport?lang=en"
     browser.visit(url)
-    
-    # Parse Results HTML with BeautifulSoup
     html = browser.html
-    weather_soup = BeautifulSoup(html, "html.parser")
+    soup = BeautifulSoup(html_image, 'html.parser')
+    latest_tweets = soup.find("span",text=re.compile(r"InSight")).text
+    return latest_tweets
+
 
    # Mars Facts Web Scraper
 def mars_facts():
+   facts_url = 'http://space-facts.com/mars/'
+   browser.visit(facts_url)
+   # Find the mars facts DataFrame in the list of DataFrames as assign it to `mars_df`
+   mars_df = mars_facts_table[0]
+   # Assign the columns `['Description', 'Value']`
+   mars_df.columns = ['Description','Value']
+   # Set the index to the `Description` column without row indexing
+   mars_df.set_index('Description', inplace=True)
+   # Save html code to folder Assets
+   html_table = mars_df.to_html()
+   return html_table
 
    # Mars Hemispheres Web Scraper
 def hemisphere(browser):
     # Visit the USGS Astrogeology Science Center Site
     url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
     browser.visit(url)   
+    #Find the links to hemispheres
+    all_links = browser.find_by_css("a.product-item h3")
+    hemispheres = []
+    for i,link in enumerate(all_links):
+    hemisphere={}
+    browser.find_by_css("a.product-item h3")[i].click()
+    hemisphere["url"] = browser.links.find_by_partial_text("Sample").first["href"]
+    hemisphere["title"] = browser.find_by_css("h2.title").text
+    hemispheres.append(hemisphere)
+    browser.back()
+    return hemispheres
